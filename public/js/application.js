fa8d62fa915711f11236c64127ce77fb78dd2c20
Aslonski <slonski.andrey@gmail.com>
2016-05-20 10:21:44 -0500

    Add working links with sc player

--- public/js/application.js
+++ public/js/application.js
@@ -4,71 +4,35 @@ $(document).ready(function() {
     client_id: '137c00407d39811965ebfce7da0f1829'
   });
 
-  var track_url = 'http://soundcloud.com/droshik/warm-moments';
-  SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
-    // console.log('oEmbed response: ', oEmbed);
-    $('#sc-player').append(oEmbed.html);
-    $('iframe').css('width', '50%').css('height', 150)
-  });
+  var playTrack = function(url) {
+    var track_url = url;
+    SC.oEmbed(track_url, { auto_play: false }).then(function(oEmbed) {
+      $('iframe').remove();
+      $('#sc-player').append(oEmbed.html);
+      $('iframe').css('width', '50%').css('height', 150)
+    });
+  };
 
-    var data = { client_id: "137c00407d39811965ebfce7da0f1829" };
+  var data = { client_id: "137c00407d39811965ebfce7da0f1829" };
   $.ajax({
     method: 'get',
     dataType: 'json',
-    url: 'https://api.soundcloud.com/users/3449496/tracks.json',
+    url: 'https://api.soundcloud.com/users/droshik/tracks.json',
     data: data
-  }).done(function(response){
-    console.log(response);
-    response.forEach(function(track){
-    console.log(track);
-    $('#status').append(track.title) + "<br>";
 
-    });
+  }).done(function(response){
+    response.forEach(function(track){
+    $('#track-list').append(`<p> <a href='http://soundcloud.com/droshik/${track.permalink}'>${track.title}</a></p>`);
+
   });
 
+  $('#track-list').on('click', 'a', function(event){
+    event.preventDefault();
+    var track = $(this).attr('href');
+    playTrack(track);
+  });
 
-
-// $(document).ready(
-//   /* This is the function that will get executed after the DOM is fully loaded */
-//   function () {
-//     /* set user we wish to see trackList for */
-//     var USER = "droshik"
-//     /* set user name on DOM */
-//     $("#user").html(USER);
-//     SC.connect().then(function(){
-//       SC.get('/me');
-//     }).then(function(response) {
-//       console.log(response);
-//     })
-//     /* Use soundcloud API to get track list */
-//     // SC.get("/users/"+USER+"/tracks", {limit: 100}, function(tracks){
-//     //   var trackList = ""
-//     //   console.log(tracks)
-//     //   for (var i = 0; i < tracks.length; i++) {
-//     //     // console.log(tracks[i]);
-//     //     /* populate trackList html */
-//     //     // trackList += tracks[i].title + "<br>";
-//     //   }
-//     //   // $("#status").append(trackList);
-//     // });
-//   }
-// );
-
-//   // $(document).ready(function() {
-//   //   var widget = SC.Widget(document.getElementById('soundcloud_widget'));
-//   //   widget.bind(SC.Widget.Events.READY, function() {
-//   //   console.log('Ready...');
-//   // });
-
-
-//   // $(document).keyup(function(event) {
-//   //   event.preventDefault();
-//   //   if (event.keyCode == 32) {
-//   //    var widget = SC.Widget(document.getElementById('.playButton'));
-//   //    widget.bind(toggle());
-//   // }
-
-//   // });
+  });
 
 });
 
